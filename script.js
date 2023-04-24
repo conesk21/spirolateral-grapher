f6698b403ac7decf7a0185ee9e0ecc23c13a8c08
conesk21 <katie.s.cones@outlook.com>
2023-04-24 21:48:16 +0300

    add centering feature to draw graph, fix bug in graphing due to difference in canvas and cartesian coordinate system

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
+
 //directional array is right, up, down, even idexes affect x (the 0th element of the coordinate), odd affect y (the 1st element of the coordinate)
 const directArray = [[0,1], [1,1], [0,-1], [1,-1]]
-const center = [410,410]
-
-var canvas = document.querySelector('canvas');
+const canDirectArray = [[0,1], [1,-1], [0,-1], [1,1]]
+const center = [380,380]
+const canvas = document.querySelector("#upperCanvas");
+const grid = document.querySelector("#lowerCanvas")
 
 function getIterationEnd(array){
     currentPos = [0,0]
@@ -15,28 +17,12 @@ function getIterationEnd(array){
 }
 
 function getCenterPoint(array){
-    let firstIterEnd = getIterationEnd(array)
     let x = 0 
     let y = 0
-        if(array.length %4 ===1){
-            x = firstIterEnd[0]-firstIterEnd[1]
-            y = firstIterEnd[1]+firstIterEnd[0]
-      
-        } else if (array.length%4===3){
-            x = firstIterEnd[0]+firstIterEnd[1]
-            y = firstIterEnd[1]-firstIterEnd[0]
-      
-        } else if (array.length%4===2){
-            x = firstIterEnd[0]
-            y = firstIterEnd[1]
-        
-        } else {
-            let extremes = getCorners(array,4)
-            x = extremes[0][0] + extremes[1][0]
-            y = extremes[0][1] + extremes[1][1]
-
-        }
-        return [x/2, y/2]
+    let extremes = getCorners(array,4)
+    x = extremes[0][0] + extremes[1][0]
+    y = extremes[0][1] + extremes[1][1]
+    return [x/2, y/2]
     }
 
 
@@ -67,12 +53,9 @@ function getCorners(array,iterations){
 
 }
 
-function getBoundingBoxSize(array){
-    let height = 0 
-    let width = 0
-    let extremes = getCorners(array,4)
-    width = Math.abs(extremes[0][0]-extremes[1][0])
-    height = Math.abs(extremes[0][1]-extremes[1][1])
+function getBoundingBoxSize(extremes){
+    let width = Math.abs(extremes[0][0]-extremes[1][0])
+    let height = Math.abs(extremes[0][1]-extremes[1][1])
     return Math.max(width,height)
 }
 
@@ -91,15 +74,17 @@ function getValArray(string){
     return valueArray
 }
 
-function drawGraph(array){
+function drawGraph(array,scaleFactor){
     var drawer = canvas.getContext("2d")
-    drawer.moveTo(center[0],center[1])
-    drawer.fillStyle= "rgba(0,0,0,0)"
-    let currentPos = center
+    drawer.lineCap = "square"
+    drawer.translate(center[0],center[1])
+    let graphCenter = getCenterPoint(array)
+    let currentPos = [-(graphCenter[0]*scaleFactor), (graphCenter[1]*scaleFactor)]
+    drawer.moveTo(currentPos[0],currentPos[1])
     for (let j=0; j<getNumberofIterations(array.length);j++){
         for(let i=0; i<array.length;i++){
             let index = (i + j*array.length) % 4
-            currentPos[directArray[index][0]] += directArray[index][1]*array[i] *40
+            currentPos[canDirectArray[index][0]] += canDirectArray[index][1]*array[i] *scaleFactor
             drawer.lineTo(currentPos[0],currentPos[1])
             drawer.stroke()
         }
@@ -115,5 +100,12 @@ function handleGraph(){
 }
 
 
+let test = grid.getContext("2d")
+test.moveTo(0,380)
+test.lineTo(760,380)
+test.moveTo(380,0)
+test.lineTo(380,760)
+test.stroke()
 
+drawGraph([1,3,2,4,5],40)
 
